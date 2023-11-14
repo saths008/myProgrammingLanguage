ff7478bbe9c11e934c26cc2a4881b86abe1cadd1
Saath Satheeshkumar <saathsatheesh@gmail.com>
2023-11-14 11:21:12 +0000

    refactor: Use using statements for vector, shared ptr in expr

--- src/expr.cpp
+++ src/expr.cpp
@@ -3,51 +3,51 @@
 #include <memory>
 #include <string>
 #include <vector>
-using std::cout, std::endl;
+using std::cout, std::shared_ptr, std::string, std::vector;
 // Binary class
-Binary::Binary(std::shared_ptr<const Expr> leftParam,
-               std::shared_ptr<const Expr> rightParam,
-               std::shared_ptr<const Token> opParam)
+Binary::Binary(shared_ptr<const Expr> leftParam,
+               shared_ptr<const Expr> rightParam,
+               shared_ptr<const Token> opParam)
     : left(leftParam), right(rightParam), op(opParam) {}
-std::shared_ptr<const Token> Binary::getOp() const { return this->op; };
-std::shared_ptr<const Expr> Binary::getLeft() const { return this->left; }
-std::shared_ptr<const Expr> Binary::getRight() const { return this->right; }
-void Binary::accept(const std::shared_ptr<ExprVisitor> visitor) const {
+shared_ptr<const Token> Binary::getOp() const { return this->op; };
+shared_ptr<const Expr> Binary::getLeft() const { return this->left; }
+shared_ptr<const Expr> Binary::getRight() const { return this->right; }
+void Binary::accept(const shared_ptr<ExprVisitor> visitor) const {
   visitor->visit(*this);
 };
 // Grouping class
-Grouping::Grouping(std::shared_ptr<const Expr> exprParam) : expr(exprParam) {}
-std::shared_ptr<const Expr> Grouping::getExpr() const { return this->expr; }
-void Grouping::accept(const std::shared_ptr<ExprVisitor> visitor) const {
+Grouping::Grouping(shared_ptr<const Expr> exprParam) : expr(exprParam) {}
+shared_ptr<const Expr> Grouping::getExpr() const { return this->expr; }
+void Grouping::accept(const shared_ptr<ExprVisitor> visitor) const {
   visitor->visit(*this);
 };
 // Unary class
-Unary::Unary(std::shared_ptr<const Expr> exprParam,
-             std::shared_ptr<const Token> tokenParam)
+Unary::Unary(shared_ptr<const Expr> exprParam,
+             shared_ptr<const Token> tokenParam)
     : expr(exprParam), token(tokenParam){};
-std::shared_ptr<const Expr> Unary::getExpr() const { return this->expr; }
-void Unary::accept(const std::shared_ptr<ExprVisitor> visitor) const {
+shared_ptr<const Expr> Unary::getExpr() const { return this->expr; }
+void Unary::accept(const shared_ptr<ExprVisitor> visitor) const {
   visitor->visit(*this);
 };
-std::shared_ptr<const Token> Unary::getToken() const { return this->token; };
+shared_ptr<const Token> Unary::getToken() const { return this->token; };
 // Literal class
-Literal::Literal(std::string valueParam) : value(valueParam) {}
-std::string Literal::getValue() const { return this->value; }
-void Literal::accept(const std::shared_ptr<ExprVisitor> visitor) const {
+Literal::Literal(string valueParam) : value(valueParam) {}
+string Literal::getValue() const { return this->value; }
+void Literal::accept(const shared_ptr<ExprVisitor> visitor) const {
   visitor->visit(*this);
 };
 // accept methods
 // PrintVisitor methods
 void PrintVisitor::visit(const Grouping &grouping) const {
-  std::vector<std::shared_ptr<const Expr>> exprs;
-  std::shared_ptr<const Expr> expr = grouping.getExpr();
+  vector<shared_ptr<const Expr>> exprs;
+  shared_ptr<const Expr> expr = grouping.getExpr();
   exprs.push_back(expr);
   paranthesize("Grouping", exprs);
 };
 void PrintVisitor::visit(const Unary &unary) const {
-  std::vector<std::shared_ptr<const Expr>> exprs;
-  std::shared_ptr<const Expr> expr = unary.getExpr();
-  std::string tokenType = unary.getToken()->stringifyTokenType();
+  vector<shared_ptr<const Expr>> exprs;
+  shared_ptr<const Expr> expr = unary.getExpr();
+  string tokenType = unary.getToken()->stringifyTokenType();
   exprs.push_back(expr);
   paranthesize(tokenType, exprs);
 
@@ -55,11 +55,11 @@ void PrintVisitor::visit(const Unary &unary) const {
 };
 void PrintVisitor::visit(const Binary &binary) const {
 
-  std::vector<std::shared_ptr<const Expr>> exprs;
-  std::shared_ptr<const Expr> leftExprPtr = binary.getLeft();
-  std::shared_ptr<const Expr> rightExprPtr = binary.getRight();
-  std::shared_ptr<const Token> opTokenPtr = binary.getOp();
-  std::string tokenType = opTokenPtr->stringifyTokenType();
+  vector<shared_ptr<const Expr>> exprs;
+  shared_ptr<const Expr> leftExprPtr = binary.getLeft();
+  shared_ptr<const Expr> rightExprPtr = binary.getRight();
+  shared_ptr<const Token> opTokenPtr = binary.getOp();
+  string tokenType = opTokenPtr->stringifyTokenType();
   exprs.push_back(leftExprPtr);
   exprs.push_back(rightExprPtr);
   paranthesize(tokenType, exprs);
@@ -67,17 +67,15 @@ void PrintVisitor::visit(const Binary &binary) const {
   // cout << "Binary" << endl;
 };
 void PrintVisitor::visit(const Literal &literal) const {
-  std::vector<std::shared_ptr<const Expr>> exprs;
+  vector<shared_ptr<const Expr>> exprs;
   paranthesize(literal.getValue(), exprs);
   // cout << "Literal" << endl;
 };
 // paranthesize
-void paranthesize(const std::string &name,
-                  std::vector<std::shared_ptr<const Expr>> &exprs) {
+void paranthesize(const string &name, vector<shared_ptr<const Expr>> &exprs) {
   cout << "(" << name;
-  std::shared_ptr<ExprVisitor> exprVisitorPtr =
-      std::make_shared<PrintVisitor>();
-  for (const std::shared_ptr<const Expr> &expr : exprs) {
+  shared_ptr<ExprVisitor> exprVisitorPtr = std::make_shared<PrintVisitor>();
+  for (const shared_ptr<const Expr> &expr : exprs) {
 
     cout << " ";
     expr->accept(exprVisitorPtr);
