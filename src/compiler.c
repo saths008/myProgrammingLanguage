c066e0781fefc696e68555803d94ab138b6bc04e
Saath Satheeshkumar <saathsatheesh@gmail.com>
2024-02-07 16:51:10 +0000

    feat: Add some emitting byte functions

--- src/compiler.c
+++ src/compiler.c
@@ -1,7 +1,14 @@
 #include "compiler.h"
+#include "bytecodeSeq.h"
 #include "scanner.h"
 #include <stdio.h>
+#include <stdlib.h>
 
+BytecodeSeq *compilingBytecodeSeq;
+static BytecodeSeq *currentBytecodeSeq() { return compilingBytecodeSeq; }
+static void emitByte(Parser *parser, uint8_t byte) {
+  writeBytecodeSeq(currentBytecodeSeq(), byte, parser->previous.line);
+}
 static void consume(Scanner *scanner, Parser *parser, TokenType type,
                     const char *message) {
   if (parser->current.type == type) {
@@ -50,13 +57,39 @@ void initParser(Parser *parser) {
   parser->hadError = false;
   parser->panicMode = false;
 }
-// bool compile(const char *sourceCode, BytecodeSeq *bytecodeSeq) {
-//   Scanner scanner;
-//   initScanner(&scanner, sourceCode);
-//   Parser parser;
-//   initParser(&parser);
-//   advanceToken(&scanner, &parser);
-//   expression();
-//   consume(&scanner, &parser, TOKEN_EOF, "Expect end of expression.");
-//   return !parser.hadError;
-// }
+static void emitReturn(Parser *parser) { emitByte(parser, OP_RETURN); }
+
+static uint8_t makeConstant(Parser *parser, Value value) {
+  int constant = addConstant(currentBytecodeSeq(), value);
+  if (constant > UINT8_MAX) {
+    error(parser, "Too many constants in one chunk.");
+    return 0;
+  } else {
+    return (uint8_t)constant;
+  }
+}
+static void emitTwoBytes(Parser *parser, uint8_t byte1, uint8_t byte2) {
+  emitByte(parser, byte1);
+  emitByte(parser, byte2);
+}
+static void emitConstant(Parser *parser, Value value) {
+  emitTwoBytes(parser, OP_CONSTANT, makeConstant(parser, value));
+}
+
+void endCompiler(Parser *parser) { emitByte(parser, OP_RETURN); }
+bool compile(const char *sourceCode, BytecodeSeq *bytecodeSeq) {
+  Scanner scanner;
+  initScanner(&scanner, sourceCode);
+  compilingBytecodeSeq = bytecodeSeq;
+  Parser parser;
+  initParser(&parser);
+  advanceToken(&scanner, &parser);
+  expression();
+  consume(&scanner, &parser, TOKEN_EOF, "Expect end of expression.");
+  endCompiler(&parser);
+  return !parser.hadError;
+}
+static void number(Parser *parser) {
+  double value = strtod(parser->previous.start, NULL);
+  emitConstant(value);
+}
